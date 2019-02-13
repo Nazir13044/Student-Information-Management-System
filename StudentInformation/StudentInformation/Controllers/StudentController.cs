ea30166dcf6a32b70c23633a085f7d0896fe92b2
MD. Nazirul Ahsan <42348515+Nazir13044@users.noreply.github.com>
2019-02-13 22:36:29 +0600

    first commit

---
+++ StudentInformation/StudentInformation/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentInformation.Models;
+using StudentInformation.Repository;
+
+namespace StudentInformation.Controllers
+{
+
+
+    public class StudentController : Controller
+    {
+       
+        
+        private readonly StudentRepository _information = new StudentRepository();
+        public ActionResult StudentInfo()
+        {
+            return View();
+        }
+
+       
+        public JsonResult InsertStudentInfo(List<Student> stu)
+        {
+
+            var info = _information.Insert(stu);
+
+            return Json(info);
+
+
+        }
+	}
+}
---
+++ StudentInformation/StudentInformation/Repository/StudentRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.Mvc;
+using StudentInformation.Models;
+
+namespace StudentInformation.Repository
+{
+
+    public class StudentRepository
+    {
+        private readonly StudentDBEntities _entities = new StudentDBEntities();
+        [OperationBehavior(TransactionScopeRequired = true, TransactionAutoComplete = true)]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        public object Insert(List<Student> stusList)
+        {
+           
+                try
+                {
+                    foreach (var student in stusList)
+                    {
+                        _entities.Students.Add(student);
+                        _entities.SaveChanges();
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                     
+                    throw exception;
+                    return false;
+                }
+            
+            return true;
+
+        }
+    }
+}
